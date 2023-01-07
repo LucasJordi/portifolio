57e95222dbccbfea3516c0e61799007c08f47fbe
LucasJordi <lucasjordi24@gmail.com>
2023-01-06 21:47:44 -0300

    Change in menu component

--- src/components/menuBar/index.js
+++ src/components/menuBar/index.js
@@ -4,25 +4,80 @@ import {ReactComponent as BateryIcon} from '../../assets/icon/battery-charging-o
 import {ReactComponent as WifiIcon} from '../../assets/icon/wifi-outline.svg'
 import {ReactComponent as VolumeIcon} from '../../assets/icon/volume-medium.svg'
 import {ReactComponent as CafeOutIcon} from '../../assets/icon/cafe-outline.svg'
+import {ReactComponent as CafeIcon} from '../../assets/icon/cafe.svg'
+import {ReactComponent as CafeFillIcon} from '../../assets/icon/cafe-filled.svg'
 import { useEffect, useState } from 'react';
+import { Float } from '../float';
+import { MenuTools } from '../menuTools';
 
 
 
 export const MenuBar=()=>{
     const [date,setDate]=useState(new Date())
+    const [clickCafe,setClickCafe]=useState(false)
+    const [float,setFloat]=useState(false)
+    const [menu,setMenu]=useState(false)
+
+    const [textFloat,setTextFloat]=useState("")
+
     const dias=["Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"]
     const formatDate=()=>{
         return `${dias[date.getMonth()]} ${date.getDate()} ${date.getHours()}:${date.getMinutes()}`
     }
+    useEffect(()=>{
+        
+        window.addEventListener("click",(event)=>{
+            if(menu){
+                try{
+                    const element=document.querySelector(".mainMenuTool")
+                    let classes=["mainMenuTool","wifiAndOthers"]
+                    element.childNodes.forEach(element=>{
+                        classes.push(element.className)
+                    })
+                    const classesContain=classes.includes(event.target.className)
+                    console.log(classesContain)
+                    if(menu && !classesContain){
+                        setMenu(false)
+
+                    }
+
+                }catch(e){
+                    
+                }
+                
+
+            }
+            
+        
+        })
+    },[])
     useEffect(()=>{
         setInterval(()=>{
             setDate(new Date())
 
         },1000)
 
+
     },[])
+    const clickChangeCafe=()=>{
+        setTextFloat("Auto suspended and screensaver enabled")
+        setClickCafe(!clickCafe)
+        if(!clickCafe){
+            setFloat(true)
+
+            setTimeout(()=>{
+                setFloat(false)
+
+            },2000)
+        }else{
+            setFloat(false)
+        }
+    }
 
     return(
+        <>
+        {float&&<Float text={textFloat}/>}
+        {menu && <MenuTools/>}
         <div className="bar">
             <div class="cantos">
                 <div className='divLogo'>
@@ -41,8 +96,9 @@ export const MenuBar=()=>{
 
             </div>
             <div className="cantos flexRight">
-                <div className='othersPanel'>
-                    <CafeOutIcon className='iconCafe' />
+                <div className='othersPanel' onClick={()=>clickChangeCafe()} >
+                    {clickCafe? <CafeOutIcon className='iconCafe' />:
+                    <CafeIcon className='iconCafe'/>}
                 
 
                 </div>
@@ -54,7 +110,11 @@ export const MenuBar=()=>{
 
                 </div>
 
-                <div className="wifiAndOthers">
+                <div className="wifiAndOthers" onClick={()=>{
+                    setTimeout(()=>{setMenu(!menu)},500)
+                    
+                }} 
+                >
                     <WifiIcon className="iconW" />
                     <VolumeIcon className="iconW" />
                     <BateryIcon className="iconW" />
@@ -70,6 +130,7 @@ export const MenuBar=()=>{
             </div>
            
         </div>
+        </>
     )
 
 }
